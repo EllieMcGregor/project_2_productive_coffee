a297875dadc7b76fa66a1aacdbae8d5bdd099426
Sebastian Riddle <sebastian@go-thassos.gr>
2015-01-17 20:24:13 +0000

    create has-many-through associations between Shops & Facilities

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,2 +1,4 @@
 class Facility < ActiveRecord::Base
+  has_many :shops_facility
+  has_many :shops, through: :shops_facility
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,5 @@
 class Shop < ActiveRecord::Base
   belongs_to :user
+  has_many :shops_facility
+  has_many :facilities, through: :shops_facility
 end
