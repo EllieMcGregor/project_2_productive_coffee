a95935dd7de1c3df52ab5eada5ad4dc001e69227
Sebastian Riddle <sebastian@go-thassos.gr>
2015-01-17 19:42:25 +0000

    update user information in database with information provided by google auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,25 @@ class User < ActiveRecord::Base
     if user = User.find_by_email(auth.info.email)
       user.provider = auth.provider
       user.uid = auth.uid
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.user_image = get_image_path_without_params(auth.info.image)
       user
     else
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.provider = auth.provider
         user.uid = auth.uid
+        user.first_name = auth.info.first_name
+        user.last_name = auth.info.last_name
+        user.user_image = get_image_path_without_params(auth.info.image)
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
       end
     end
+  end
+
+  private
+  def self.get_image_path_without_params(url)
+    url.split("?").first
   end
 end
