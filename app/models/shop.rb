3fc22a96e00669aff9306feba05bcc0ba9919912
Sebastian Riddle <sebastian@go-thassos.gr>
2015-01-18 16:04:55 +0000

    add validation for Shop model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,8 @@ class Shop < ActiveRecord::Base
   belongs_to :user
   has_many :shops_facility
   has_many :facilities, through: :shops_facility
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :user, presence: true
 end
