51599664c44a6312cb355c00c214e3a3b4524ea7
EllieMcGregor <ellie.mcgregor@gmail.com>
2015-01-18 14:46:21 +0000

    added contents of comments controller that the gem suggests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,1 +1,23 @@
-comments_controller.rb
+class CommentsController < ApplicationController
+  load_and_authorize_resource
+  
+
+  def create
+    commentable = Post.create
+    comment = commentable.comments.create
+    comment.title = "First comment."
+    comment.comment = "This is the first comment."
+    comment.save
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to ...
+  end
+
+  def comment_params
+    params.require(:comment).permit(:title, :comment, :user_id, :shop_id)
+  end
+
+end
