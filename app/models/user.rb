43b6dd770521f2a513108047b045cf1d8747f64b
Sebastian Riddle <sebastian@go-thassos.gr>
2015-01-18 15:59:33 +0000

    add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
   has_many :shops
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  
+  validates :password, presence: true  
+  validates :password, length: { in: 8..20 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
