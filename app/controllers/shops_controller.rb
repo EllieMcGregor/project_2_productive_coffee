fbfd3f2041030866b3d7cc0ed8cd2584c9635aab
Sebastian Riddle <sebastian@go-thassos.gr>
2015-01-19 19:03:03 +0000

    set before_action for Shops controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,11 @@
 class ShopsController < ApplicationController
+  before_action :set_shop, only: [:show, :edit, :update, :destroy]
+
   def index
     @shops = Shop.all
   end
+  private
+  def set_shop
+    @shop = Shop.find(params[:id])
   end
 end
