a3d96d7f76def800e9bee12438fbdf018c50c725
Sebastian Riddle <sebastian@go-thassos.gr>
2015-01-20 09:52:00 +0000

    update seed file with a dummy shop description

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,18 +30,18 @@ facilities << Facility.create!(name: "Conference Room")
 facilities << Facility.create!(name: "Meals")
 
 ellie = User.create!(first_name: "Ellie", last_name: "McGregor", email: "ellie.mcgregor@gmail.com", password: "password", role: :admin)
-ellie.shops.create!(name:"#{Faker::Company.name}", email: "#{Faker::Internet.email}", phone: "#{Faker::PhoneNumber.phone_number}", latitude: rand(minLat..maxLat), longitude: rand(minLng..maxLng), street: street, city: city, postcode: postcode, country: country)
+ellie.shops.create!(name:"#{Faker::Company.name}", description: "#{Faker::Lorem.paragraph}", email: "#{Faker::Internet.email}", phone: "#{Faker::PhoneNumber.phone_number}", latitude: rand(minLat..maxLat), longitude: rand(minLng..maxLng), street: street, city: city, postcode: postcode, country: country)
 
 daniel = User.create!(first_name: "Daniel", last_name: "Easterman", email: "daniel.easterman@gmail.com", password: "password", role: :admin)
-daniel.shops.create!(name:"#{Faker::Company.name}", email: "#{Faker::Internet.email}", phone: "#{Faker::PhoneNumber.phone_number}", latitude: rand(minLat..maxLat), longitude: rand(minLng..maxLng), street: street, city: city, postcode: postcode, country: country)
+daniel.shops.create!(name:"#{Faker::Company.name}", description: "#{Faker::Lorem.paragraph}", email: "#{Faker::Internet.email}", phone: "#{Faker::PhoneNumber.phone_number}", latitude: rand(minLat..maxLat), longitude: rand(minLng..maxLng), street: street, city: city, postcode: postcode, country: country)
 
 sebastian = User.create!(first_name: "Sebastian", last_name: "Riddle", email: "sebastian.riddle@gmail.com", password: "password", role: :admin)
-sebastian.shops.create!(name:"#{Faker::Company.name}", email: "#{Faker::Internet.email}", phone: "#{Faker::PhoneNumber.phone_number}", latitude: rand(minLat..maxLat), longitude: rand(minLng..maxLng), street: street, city: city, postcode: postcode, country: country)
+sebastian.shops.create!(name:"#{Faker::Company.name}", description: "#{Faker::Lorem.paragraph}", email: "#{Faker::Internet.email}", phone: "#{Faker::PhoneNumber.phone_number}", latitude: rand(minLat..maxLat), longitude: rand(minLng..maxLng), street: street, city: city, postcode: postcode, country: country)
 
 10.times do
   user = User.create!(first_name: "#{Faker::Name.first_name}", last_name: "#{Faker::Name.last_name}", email: "#{Faker::Internet.email}", password: "password", user_image: "#{Faker::Avatar.image}", role: :normal)
   
-  user.shops.create!(name:"#{Faker::Company.name}", email: "#{Faker::Internet.email}", phone: "#{Faker::PhoneNumber.phone_number}", latitude: rand(minLat..maxLat), longitude: rand(minLng..maxLng), street: street, city: city, postcode: postcode, country: country)
+  user.shops.create!(name:"#{Faker::Company.name}", description: "#{Faker::Lorem.paragraph}", email: "#{Faker::Internet.email}", phone: "#{Faker::PhoneNumber.phone_number}", latitude: rand(minLat..maxLat), longitude: rand(minLng..maxLng), street: street, city: city, postcode: postcode, country: country)
   
   users_shop = user.shops.first
 
