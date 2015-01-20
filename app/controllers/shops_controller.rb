8e2993a70b385be2d074e6948db206eddc35560a
Daniel Easterman <daniel.easterman@gmail.com>
2015-01-20 11:21:01 +0000

    deletes search clear method, bad blog post tutorial example

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -8,18 +8,7 @@ class ShopsController < ApplicationController
     @q = Shop.search(params[:q])
     @shops = @q.result(distinct: true)
     respond_with(@shops)
-  end
-
-  def clear_search_index
-    if params[:search_cancel]
-        params.delete(:search_cancel)
-        if(!search_params.nil?)
-          search_params.each do |key, param|
-            search_params[key] = nil
-          end
-        end
-      end
-  end
+  end 
 
   def show
     respond_with(@shop)
