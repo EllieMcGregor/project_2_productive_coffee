d556ce9bc145e6731523fe7f9249ea1be4c31cbe
EllieMcGregor <ellie.mcgregor@gmail.com>
2015-01-22 18:36:18 +0000

    removed ghost

--- db/migrate/20150122115257_add_shop_image_to_shops.rb
+++
@@ -1,5 +0,0 @@
-class AddShopImageToShops < ActiveRecord::Migration
-  def change
-    add_column :shops, :shop_image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122115257) do
+ActiveRecord::Schema.define(version: 20150121105426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20150122115257) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "shop_image"
     t.float    "latitude"
     t.float    "longitude"
     t.string   "street"
@@ -94,7 +95,6 @@ ActiveRecord::Schema.define(version: 20150122115257) do
     t.string   "postcode"
     t.string   "country"
     t.text     "description"
-    t.string   "shop_image"
   end
 
   add_index "shops", ["user_id"], name: "index_shops_on_user_id", using: :btree
